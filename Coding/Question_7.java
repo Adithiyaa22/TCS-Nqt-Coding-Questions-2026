287e6a8e9eb3a1084a1478d49513b346dc4f9dab
Adithiyaa S <146060022+Adithiyaa22@users.noreply.github.com>
2026-04-18 13:56:51 +0530

    Implement happy-sad state simulation in MyClass
    Simulate happy-sad state transitions for 4 iterations.

--- Coding/Question_7.java
+++ Coding/Question_7.java
@@ -1,6 +1,6 @@
 /*
 📌 Question: Happy-Sad State Simulation
-
+Give class name as "MyClass"
 You are given an integer N representing the number of people initially in a happy state.
 
 In each iteration, the following transitions happen:
@@ -8,6 +8,9 @@ In each iteration, the following transitions happen:
 - 30% of happy people remain happy
 - 50% of sad people remain sad
 - 50% of sad people become happy
+
+⚙️ Task:
+Simulate the process for 4 iterations and compute the final counts.
 
 📥 Input:
 - A single integer N (initial number of happy people)
@@ -32,3 +35,19 @@ Input:
 Output:
 41 58   (or rounded as per requirement)
 */
+
+import java.util.*;
+public class MyClass{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int happy = sc.nextInt();
+    int sad = 0;
+    int new_sad = 0, new_happy = 0;
+    for(int i=1;i<=4;i++)
+      {
+         new_sad = 0.7*happy + 0.5*sad;
+        new_happy = 0.3*happy + 0.5*happy;
+      }
+    System.out.println(new_happy+" "+new_sad);
+  }
+}
