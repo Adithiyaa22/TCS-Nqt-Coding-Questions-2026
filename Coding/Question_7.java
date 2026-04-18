e14b31502cd1882e3495eba4e98dd3b71d4ff9ee
Adithiyaa S <146060022+Adithiyaa22@users.noreply.github.com>
2026-04-18 13:46:04 +0530

    Create Question_7.java

---
+++ Coding/Question_7.java
@@ -0,0 +1,34 @@
+/*
+📌 Question: Happy-Sad State Simulation
+
+You are given an integer N representing the number of people initially in a happy state.
+
+In each iteration, the following transitions happen:
+- 70% of happy people become sad
+- 30% of happy people remain happy
+- 50% of sad people remain sad
+- 50% of sad people become happy
+
+📥 Input:
+- A single integer N (initial number of happy people)
+
+📤 Output:
+- Print two integers:
+  1. Final number of happy people
+  2. Final number of sad people
+
+🔄 Initial State:
+- Happy = N
+- Sad = 0
+
+⚠️ Note:
+- Use double for calculations if needed, then convert to integer (as per problem requirement)
+
+🧪 Example:
+
+Input:
+100
+
+Output:
+41 58   (or rounded as per requirement)
+*/
